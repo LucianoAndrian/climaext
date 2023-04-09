71f32ab545837b52140d509d9a9c907738d83762
V <44793625+LucianoAndrian@users.noreply.github.com>
2023-04-09 18:45:06 -0300

    act

--- ClimExt_P0.R
+++ ClimExt_P0.R
@@ -12,6 +12,71 @@ temp = read.table(paste(data_dir, 'edc3deuttemp2007_Resunamierda.txt', sep=''),
                   header = T, fill=T)
 
 
-my.wt = analyze.wavelet(temp[c('Age', 'Temperature')], my.series = 1, dt=1000)
+df = data.frame(c(temp$Temperature))
 
-# definir paso/s temporal --> python
+# la temperatura tiene 3 valores faltantesm de mas de 5mil, reemplazo por la media
+temp$Temperature[which(is.na(temp$Temperature))]=mean(temp$Temperature, na.rm = T)
+
+my.wt = analyze.wavelet(temp, my.series = 5, dt=1, dj = 1/10,
+                        lowerPeriod = 10, upperPeriod = 4000, n.sim = 10)
+
+pdf("temp_wavelet.pdf", width = 7, height = 5)
+wt.image(my.wt, 
+         color.key="interval", 
+         legend.params=list(lab="Espectro de poder de wavelet"),
+         siglvl=0.05, # SIGNIFICANCIA
+         col.contour = 'black',
+         plot.ridge = FALSE, #AGREGA CONTORNO EXTRA DE LOS PUNTOS DE MAX VARIANZA
+         color.palette = 'rainbow(n.levels,start=1,end=.65)' ,
+         main= 'Temp. 800yk',
+         timelab = 'Tiempo',
+         periodlab= 'Periodo')
+dev.off()
+ 
+co2 = read.table(paste(data_dir, 'edc3-composite-co2-2008-noaa.txt', sep=''), 
+                  header = T, fill=T, skip=275)
+
+
+my.wt = analyze.wavelet(co2, my.series = 2, dt=1, dj = 1/10,
+                        lowerPeriod = 10)
+
+pdf("co2_wavelet.pdf", width = 7, height = 5)
+wt.image(my.wt, 
+         color.key="interval", 
+         legend.params=list(lab="Espectro de poder de wavelet"),
+         siglvl=0.05, # SIGNIFICANCIA
+         col.contour = 'black',
+         plot.ridge = FALSE, #AGREGA CONTORNO EXTRA DE LOS PUNTOS DE MAX VARIANZA
+         main= 'CO2 800yk',
+         timelab = 'Tiempo',
+         periodlab= 'Periodo')
+dev.off()
+
+
+
+
+#- prueba Wavelet con pasos temporales distintos -#
+# carga de datos
+datos <- read.table(paste(data_dir, "Tmensual_Aeroparque_ejemplo.txt", sep = ''), sep="",header=FALSE)
+x1 = datos[,1][0:200]
+x2 = datos[,1][200:428][c(T,F)]
+x = as.data.frame(append(x1, x2))
+plot(x[,1], type='l')
+#con la función analyze.wavelet obtengo el wavelet de mis datos
+my.wt = analyze.wavelet(x, my.series=1,dt=1, lowerPeriod = 1, upperPeriod = 32)
+
+wt.image(my.wt, 
+         color.key="interval", 
+         legend.params=list(lab="Espectro de poder de wavelet"),
+         siglvl=0.05, # SIGNIFICANCIA
+         col.contour = 'black',
+         plot.ridge = FALSE, #AGREGA CONTORNO EXTRA DE LOS PUNTOS DE MAX VARIANZA
+         color.palette = 'rainbow(n.levels,start=1,end=.65)' ,
+         main= 'Temp 800yk',
+         timelab = 'Tiempo [años]',
+         periodlab= 'Periodo [años]')
+
+
+
+
+
--- ClimExt_P0.py
+++ ClimExt_P0.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 #----------------------------------------------------------------------------------------------------------------------#
 data_dir = '/home/luciano.andrian/doc/climaext/p0/data/'
 out_dir = '/home/luciano.andrian/doc/climaext/p0/'
-save = False
+save = True
+dpi = 300
 #----------------------------------------------------------------------------------------------------------------------#
 # 2
 temp = pd.read_csv(data_dir + 'edc3deuttemp2007.txt', sep='\s+', skiprows=91) # \s+ separador de más de 1 espacio
@@ -17,25 +18,31 @@ co2_age = co2.loc[:,'gas_ageBP']
 co2_data = co2.loc[:,'CO2']
 
 # Plot
-fig = plt.figure(1, figsize=(8, 4), dpi=300)
+fig = plt.figure(1, figsize=(10, 5), dpi=dpi)
 ax = fig.add_subplot(111)
 ax2 = ax.twinx()
-co2_tl = ax.plot(co2_age, co2_data, label='CO2 [ppm]', color='dodgerblue')
-temp_tl = ax2.plot(temp_age, temp_data, label='Temp. [ºC]', color='red')
+co2_tl = ax.plot(co2_age, co2_data, '-o', label='CO2 [ppm]', color='turquoise', linewidth=2,
+                 markersize=.5, markerfacecolor='k', markeredgecolor='k')
+temp_tl = ax2.plot(temp_age, temp_data, '-o', label='Temp. [ºC]', color='orange', linewidth=2,
+                   markersize=.5, markerfacecolor='k', markeredgecolor='k')
 
-ax.set_xlim(0,800000)
-xticks = np.arange(0,9e5,1e5)
+ax.set_xlim(temp_age.iloc[-1],temp_age[12])
+xticks = np.arange(0,temp_age.iloc[-1],1e5)
 xlab=[]
 for c,x in enumerate(xticks):
     xstr=format(x, '.0e').split('e')
     xlab.append(xstr[0] + '$x10^{5}$')
 ax.set_xticks(xticks, xlab)
-ax.set_ylim(0, 350)
+ax.set_xlabel('Años -->')
+
+ax.set_ylim(100, 300)
+ax.set_ylabel('CO2 [ppm]')
 ax2.set_ylim(-12,8)
+ax2.set_ylabel('Temp. [ºC]')
 
 lns = co2_tl + temp_tl
 labs = [l.get_label() for l in lns]
-ax.legend(lns, labs, loc=0)
+ax.legend(lns, labs, loc='upper left')
 
 if save:
     plt.savefig(out_dir + 'p0_c.jpg')
@@ -61,5 +68,5 @@ def CheckTimeStep(serie):
 
         diff.append(t_dif)
 
-plt.plot(CheckTimeStep(co2_age));plt.show()
-plt.plot(CheckTimeStep(temp_age));plt.show()
+plt.plot(CheckTimeStep(co2_age)[::-1]);plt.show()
+plt.plot(CheckTimeStep(temp_age)[::-1]);plt.show()
